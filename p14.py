578567f938ace96cb6ea96272028e007bbf619c2
Kevin Morris <kevr@0cost.org>
2022-08-07 16:43:18 -0700

    Problem 14
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ p14.py
@@ -0,0 +1,81 @@
+"""
+Description: ProjectEuler Problem #14
+
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains
+10 terms. Although it has not been proved yet (Collatz Problem), it is thought
+that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+
+Solution pseudocode:
+    For each positive integer over 13 and under one million
+        Find the number of memoized iterations in the Collatz sequence
+    Return the length of the longest sequence found
+
+Requirements:
+    - We must test every single number in the iteration criteria
+    - We can shortcut tests by relying on memoized lengths
+
+Copyright (C) 2022 Kevin Morris <kevr@0cost.org>
+All Rights Reserved.
+"""
+import sys
+import traceback
+
+# Global memo used for collatz sequence lengths
+memo = dict()
+
+
+def collatz_iteration(n: int) -> int:
+    if n % 2 == 0:
+        return n / 2
+    return (3 * n) + 1
+
+
+def collatz(n: int) -> int:
+    if n <= 2:
+        return n
+
+    i: int = 0  # Number of iterations
+    k: int = n
+    while k > 1:
+        if k in memo:
+            return i + memo.get(k)
+
+        i += 1
+        k = collatz_iteration(k)
+
+    memo[n] = i
+    return i
+
+
+def main():
+    longest: int = 0
+    starting: int = 0
+
+    for n in range(13, 1000000):
+        length = collatz(n)
+        if length > longest:
+            longest = length
+            starting = n
+
+    print(f"{starting} = {longest}")
+    return 0
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except Exception:
+        traceback.print_exc()
+        sys.exit(1)
